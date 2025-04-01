42a67d69216dac8cadc3eee17b8bb47c2536fce0
Xiaoli Shen <alabebop@gmail.com>
2025-04-01 07:16:56 +0000

    add profiling LLM

---
+++ profile_llm.py
@@ -0,0 +1,21 @@
+import torch
+from transformers import AutoModelForCausalLM
+
+
+# start recording memory snapshot history
+torch.cuda.memory._record_memory_history(max_entries=100000)
+
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B").to("cuda")
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+for _ in range(3):
+    inputs = torch.randint(0, 100, (16, 256), device="cuda") # dummy input
+    loss = torch.mean(model(inputs).logits) # dummy loss
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+# dump memory snapshot history to a pkl file and stop recording
+torch.cuda.memory._dump_snapshot("profile_llm.pkl")
+torch.cuda.memory._record_memory_history(enabled=None)
+                                        
